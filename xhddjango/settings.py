d7a8dd139c5328e3c6e8a619a8848ddcb1634162
jiangwn815 <jiangwn815@hotmail.com>
2017-02-13 23:30:23 +0800

    django

--- xhddjango/settings.py
+++ xhddjango/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'xhddjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'django_test',
+        'USER': 'root',
+        'PASSWORD': 'mypassword',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
