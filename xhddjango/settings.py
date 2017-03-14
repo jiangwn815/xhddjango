8e19ab94cf0d5faa14fe5d344dbb5c9251c140e6
jiang <jiangwn815@hotmail.com>
2017-03-14 16:24:44 +0800

    template

--- xhddjango/settings.py
+++ xhddjango/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'mainpages.apps.MainpagesConfig',
+    'xhddjango.apps.XhddjangoConfig',
+    'ydys.apps.YdysConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
