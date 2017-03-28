1042710e462c373f5219d5deda1adc0be0034ba8
jiang <jiangwn815@hotmail.com>
2017-03-28 16:48:55 +0800

    git ignore

--- mainpages/static/mainpages/js/register.js
+++ mainpages/static/mainpages/js/register.js
@@ -3,26 +3,38 @@
  */
 'use strict';
 function register() {
-    $('#onlineRegister').click(function () {
-        $('#registerOptions').css("display","none");
-        $('#userInfo').css("display","block");
-    });
+
     $('#submit-btn').click(function () {
         var validation = true;
+        if($('#userName').val()=== ""){
+            validation = false;
+            $('#userName').next().css('visibility','visible');
+            $('#userName').parent().addClass('has-danger');
+        }
+
         if($('#password').val()!==$('#passwordConfirmation').val()){
+            console.log("#password value"+$('#password').val());
             validation = false;
         }
-        if($('#userName')===null){
+        console.log("userName value"+$('#userName').val());
+        if($('#userName').val()=== ""){
+            console.log("validation userName value"+$('#userName').val());
             validation = false;
             
         }
+
         if(validation){
-            $('#registerForm').submit();
+            console.log("validation");
+            //$('#registerForm').submit();
         }
     });
 }
 
 
 $(function(){
+    $('#onlineRegister').click(function () {
+        $('#registerOptions').css("display","none");
+        $('#userInfo').css("display","block");
+    });
     register();
 });
