079539ffa5eaa8405f07477ce200d1ddb2c33c75
jiang <jiangwn815@hotmail.com>
2017-03-31 13:53:46 +0800

    git ignore

--- mainpages/admin.py
+++ mainpages/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import User, Order
+from .models import User, Order, YdysTask
 
 admin.site.register(User)
 admin.site.register(Order)
+admin.site.register(YdysTask)
--- mainpages/models.py
+++ mainpages/models.py
@@ -5,6 +5,8 @@ from django.db import models
 
 
 class User(models.Model):
+    # null 是针对数据库而言，如果 null=True, 表示数据库的该字段可以为空。
+    # blank 是针对表单的，如果 blank=True，表示你的表单填写该字段的时候可以不填
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True)
@@ -44,3 +46,18 @@ class Order(models.Model):
 
     def __str__(self):
         return self.out_trade_no
+
+
+class YdysTask(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    startAt = models.DateTimeField(null=True, blank=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+    taskName = models.CharField(max_length=64, null=True, blank=True)  # 交易类型
+    taskContent = models.CharField(max_length=512, null=True, blank=True)  # 交易类型
+    taskStatus = models.CharField(max_length=6, null=True, blank=True)  # 交易类型
+    senderNumber = models.CharField(max_length=32, null=True, blank=True)  # 支付方式的支付流水
+    receiverNumber = models.CharField(max_length=1024, null=True, blank=True)
+
+    def __str__(self):
+        return self.taskName
